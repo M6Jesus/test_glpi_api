8963234b5d0530a7ab02c74fdc9b92f23ec46b17
pamela <fokopamela@yahoo.fr>
2023-04-05 12:46:41 +0200

    retrait des suites

--- src/test/java/org/test/api/GlpiApiAll.java
+++ src/test/java/org/test/api/GlpiApiAll.java
@@ -3,7 +3,7 @@ package org.test.api;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({Endpoint.class, GlpiApiCallT.class})
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({Endpoint.class, GlpiApiCallT.class})
 public class GlpiApiAll {
 }
